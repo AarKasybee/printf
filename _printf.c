624c3603d6333171774c8b32fb53a4efa2c0dd7d
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-27 02:45:03 -0700

    task 0 printf

---
+++ _printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _printf - Printf look-alike function
+ * @format: input format
+ * Return: the number of characters printed 
+ */
+int _printf(const char *format, ...)
+{
+	int num_char = 0;
+	va_list arg_list;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg_list, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			switch (*(++format))
+			{
+				case 'c':
+					putchar(va_arg(arg_list, int));
+					num_char++;
+					break;
+				case 's':
+					puts(va_arg(arg_list, char *));
+					num_char++;
+					break;
+				default:
+					putchar(*format);
+					num_char++;
+			}
+		}
+		else
+		{
+			putchar(*format);
+		}
+		format++;
+	}
+	va_end(arg_list);
+	return (num_char);
+}
